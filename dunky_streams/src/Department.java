63fbd068b0e14f32b659447bb61c83ea34ea4eb1
dunky-star <dunkygeoffrey39@gmail.com>
2023-07-03 16:42:36 +0300

    Java 8 streams

--- dunky_streams/src/Department.java
+++ dunky_streams/src/Department.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Department {
     private String name;
     private List<Employee> employees;
+
+    public Department(String name){
+        this.name = name;
+        employees = new ArrayList<>();
+    }
 }
